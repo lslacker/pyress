00a4dd7c593cefee4156c538afc67865225de403
lslacker <mai.luan@gmail.com>
2016-04-19 13:43:46 +1000

    Update client.py
    Ported to Python3

--- iress/client.py
+++ iress/client.py
@@ -26,17 +26,14 @@ import time
 import logging
 
 from win32com import client
-from typelibhelper import EnsureLatestVersion
 
 log = logging.getLogger(__name__)
 
 # Generate and import Python modules for the Iress typelibs.
-DfsCmd = EnsureLatestVersion('{96EB07E1-03D0-11CF-B214-00AA002F2ED9}')
-DfsPrice = EnsureLatestVersion('{DF120F00-A275-11D1-A122-0000F82508F6}')
-DfsSec = EnsureLatestVersion('{AF802C80-B975-11D1-A138-0000F82508F6}')
-DfsIndicate = EnsureLatestVersion('{9339DB61-1602-11D2-8FC4-0000F824C8AA}')
-DfsTimeSeries = EnsureLatestVersion('{4832E620-C2AA-11D1-A143-0000F82508F6}')
-DfsText = EnsureLatestVersion('{4FF26370-DEF3-11D1-A15F-0000F82508F6}')
+DfsCmd = client.gencache.EnsureModule('{96EB07E1-03D0-11CF-B214-00AA002F2ED9}', 0, 141, 13001)
+DfsPrice = client.gencache.EnsureModule('{DF120F00-A275-11D1-A122-0000F82508F6}', 0, 141, 1300)
+DfsSec = client.gencache.EnsureModule('{AF802C80-B975-11D1-A138-0000F82508F6}', 0, 141, 1300)
+DfsIndicate = client.gencache.EnsureModule('{9339DB61-1602-11D2-8FC4-0000F824C8AA}', 0, 141, 1300)
 
 # Default timeout when fetching data from Iress.
 DEFAULT_DATA_TIMEOUT = 15
@@ -44,121 +41,8 @@ DEFAULT_DATA_TIMEOUT = 15
 # Number of seconds to wait between polling data object's state.
 REQUEST_POLL_INTERVAL = 0.2
 
-class IressError(StandardError):
+class IressError(Exception):
     pass
-
-class IressRequestWrapper(object):
-    """
-    Abstract class that wraps an Iress automation
-    CommandObject with a nicer Python interface.
-
-    """
-    def __init__(self, request):
-        self._request = request
-
-    def execute(self):
-        """
-        Derived classes should put their
-        custom result handling here.
-
-        """
-        raise NotImplementedError()
-
-    def _do_request(self):
-        """
-        Executes the actual request and polls Iress until
-        all data has been received.
-
-        """
-        self._request.Request()
-        while self._wait_for_data():
-            self._request.RequestNext()
-        log.debug('%i rows returned.', self._request.RowCount)
-
-    def _wait_for_data(self, timeout=DEFAULT_DATA_TIMEOUT):
-        """
-        Waits for data to be received from Iress. Returns True if
-        another request is required.
-
-        """
-
-        running_time = 0
-
-        while self._request.state == DfsCmd.constants.DataPending:
-            time.sleep(REQUEST_POLL_INTERVAL)
-            running_time += REQUEST_POLL_INTERVAL
-            if running_time > timeout:
-                raise IressError('Timed out waiting for data.')
-
-        if self._request.state == DfsCmd.constants.DataReady:
-            # Don't need to do anything if data is ready.
-            return False
-        elif self._request.state == DfsCmd.constants.Error:
-            raise IressError(self._request.Error)
-        elif self._request.state == DfsCmd.constants.DataIncomplete:
-            raise IressError('Data was incomplete at the time of request.')
-        elif self._request.state == DfsCmd.constants.DataMorePending:
-            log.debug('More data required.')
-            return True
-        else:
-            raise IressError('Unkown Error.')
-
-class DefaultRequestWrapper(IressRequestWrapper):
-    """
-    Returns the results of an Iress DataObject
-    query as a dictionary.
-
-    """
-    def execute(self):
-        self._do_request()
-
-        results = []
-        if self._request.RowCount > 0:
-            fields = self._request.AvailableFields(False)
-            results = [dict(zip(fields, row)) for row in self._request.GetRows]
-        return results
-
-class DataTextRequestWrapper(IressRequestWrapper):
-    """
-    Returns the result of an Iress DataText query
-    as a list of strings.
-
-    """
-    def execute(self):
-        self._do_request()
-        return [self._request.TextLine(i) for i in range(self._request.RowCount)]
-
-class DataTextHeadlineRequestWrapper(IressRequestWrapper):
-    """
-    Returns the result of an Iress DataTextHeadline
-    query with special handing for the DataText
-    property. 
-
-    Each dictionary in the list of dictionaries returned
-    from execute() will contain a DataTextRequestWrapper
-    at the 'DataText' key. The rows in the data text
-    can be accessed by calling the execute method on
-    this wrapper object.
-
-    """
-    def execute(self):
-        self._do_request()
-
-        results = []
-        if self._request.RowCount > 0:
-            fields = self._request.AvailableFields(False)
-            for index, row in enumerate(self._request.GetRows):
-                result = dict(zip(fields, row))
-                result['DataText'] = DataTextRequestWrapper(self._request.DataText(index))
-                results.append(result)
-        return results
-
-# Specific Iress requests require special handling to extract all necessary
-# data. This dictionary maps the request types to their special
-# handling class.
-REQUEST_WRAPPER_OVERRIDES = {
-    DfsText.constants.dfsDataTextHeadline : DataTextHeadlineRequestWrapper,
-}
 
 class IressDataClient(object):
     """
@@ -178,7 +62,7 @@ class IressDataClient(object):
 
         self._iress = client.Dispatch('Iress.Application')
 
-    def execute(self, name, params, objects=()):
+    def execute(self, name, params):
         """
         Executes a data object query. Name should be the data object
         type normally passed to CreateOb and params is a dictionary
@@ -193,27 +77,63 @@ class IressDataClient(object):
         # Create and execute a data request.
         request = self._iress.DataManager.CreateOb(name)
         request.Clear()
-        for k, v in params.iteritems():
+        for k, v in params.items():
             setattr(request, k, v) 
 
-        # Find out if we need to wrap the request in a special wrapper; otherwise
-        # just use the default wrapper.
-        wrappercls = REQUEST_WRAPPER_OVERRIDES.get(name, DefaultRequestWrapper)
-        log.debug('Using request wrapper %s.' % (wrappercls.__name__,))
-        wrapper = wrappercls(request)
+        self._do_request(request)
+        log.debug('%i rows returned.', request.RowCount)
 
-        return wrapper.execute()
-        
+        # Return the results.
+        results = []
+        if request.RowCount > 0:
+            fields = request.AvailableFields(False)
+            for row in request.GetRows:
+                results.append(dict(zip(fields, row)))
+        return results
+
+    def _do_request(self, request):
+        request.Request()
+        while self._wait_for_data(request):
+            request.RequestNext()
+
+    def _wait_for_data(self, request, timeout=DEFAULT_DATA_TIMEOUT):
+        """
+        Waits for data to be received from Iress. Returns True if
+        another request is required.
+
+        """
+
+        running_time = 0
+
+        while request.state == DfsCmd.constants.DataPending:
+            time.sleep(REQUEST_POLL_INTERVAL)
+            running_time += REQUEST_POLL_INTERVAL
+            if running_time > timeout:
+                raise IressError('Timed out waiting for data.')
+
+        if request.state == DfsCmd.constants.DataReady:
+            # Don't need to do anything if data is ready.
+            return False
+        elif request.state == DfsCmd.constants.Error:
+            raise IressError(request.Error)
+        elif request.state == DfsCmd.constants.DataIncomplete:
+            raise IressError('Data was incomplete at the time of request.')
+        elif request.state == DfsCmd.constants.DataMorePending:
+            log.debug('More data required.')
+            return True
+        else:
+            raise IressError('Unkown Error.')
+
 if __name__ == '__main__':
     iress = IressDataClient()
     iress.connect()
     logging.basicConfig(level=logging.DEBUG)
 
     r= iress.execute(DfsIndicate.constants.dfsDataHistoricalMarketCapitalisation, dict(
-        StartDate='01/05/2009',
-        EndDate='01/05/2009',
+        StartDate='20/11/2015',
+        EndDate='20/11/20',
         IndexFilter='XJO',
         Exchange='ASX',
     ))
-    print len(r)
+    print(len(r))
 
